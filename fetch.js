e8159a00930a0c64f78f2e8c7864ec1cf30cd018
Kevin Wang <kevinwang5658@gmail.com>
2021-02-16 18:39:10 -0500

    Image download code

--- fetch.js
+++ fetch.js
@@ -1,6 +1,7 @@
 import request from 'request';
 import { parse, Parser } from 'json2csv';
 import * as fs from 'fs'
+import * as ImageDownloader from 'image-downloader';
 
 const LOCAL_LOGIC_API_KEY = "f50e4e0998d261cd78160bb80709ceb785cc18556729dd737771c0cbcb54ffd5644455cf1df300499b"
 
@@ -10,28 +11,46 @@ const BRADLEY_PARAMS  = {
     PropertySearchTypeId: 1,
     HashCode: 0,
     ZoomLevel: 10,
-    LatitudeMax: 45.60307,
-    LongitudeMax: -75.15382,
-    LatitudeMin: 44.89537,
-    LongitudeMin: -76.44608,
+    LatitudeMax: 45.54154,
+    LongitudeMax: -75.11625,
+    LatitudeMin: 44.77617,
+    LongitudeMin: -76.40851,
     PriceMax: 225000,
     Sort: "6-D",
     PropertyTypeGroupID: 1,
     TransactionTypeId: 2,
 }
 
-const CAROLYN_PARAMS  = {
+const CAROLYN_PARAMS = {
     CultureId: 1,
     ApplicationId: 1,
     PropertySearchTypeId: 1,
     HashCode: 0,
-    ZoomLevel: 11,
-    LatitudeMax: 43.90135,
-    LongitudeMax: -79.05332,
-    LatitudeMin: 43.51420,
-    LongitudeMin: -79.69945,
-    PriceMin: 100000,
-    PriceMax: 1500000,
+    ZoomLevel: 13,
+    LatitudeMax: 43.51671,
+    LongitudeMax: -80.43317,
+    LatitudeMin: 43.41953,
+    LongitudeMin: -80.59471,
+    GeoIds: "g40_dpwz1wcm",
+    PriceMin: 400000,
+    PriceMax: 650000,
+    Sort: "6-D",
+    PropertyTypeGroupID: 1,
+    TransactionTypeId: 2,
+}
+
+const MARKHAM_PARAMS  = {
+    CultureId: 1,
+    ApplicationId: 1,
+    PropertySearchTypeId: 1,
+    HashCode: 0,
+    ZoomLevel: 12,
+    LatitudeMax: 43.97722,
+    LongitudeMax: -79.13795,
+    LatitudeMin: 43.78420,
+    LongitudeMin: -79.46101,
+    PriceMin: 500000,
+    PriceMax: 650000,
     Sort: "6-D",
     PropertyTypeGroupID: 1,
     TransactionTypeId: 2,
@@ -49,7 +68,7 @@ const parser = new Parser({
 
 async function getPage(number) {
     return new Promise((resolve, reject) => {
-        const params = Object.assign(BRADLEY_PARAMS, {
+        const params = Object.assign(CAROLYN_PARAMS, {
             CurrentPage: parseInt(number),
         })
         request.post('https://api37.realtor.ca/Listing.svc/PropertySearch_Post',
@@ -102,8 +121,11 @@ function writeCsv(generalDesc, detailsDesc) {
     const u = generalDesc;
     const v = detailsDesc;
 
+    const photoPath = v.Property.Photo?.[0]?.HighResPath;
+    const mlsNumber = u.MlsNumber;
+
     const formatted =  {
-        MlsNumber: u.MlsNumber,
+        MlsNumber: mlsNumber,
         PublicRemarks: u.PublicRemarks,
         Bathrooms: u.Building.BathroomTotal,
         Bedrooms: u.Building.Bedrooms,
@@ -114,7 +136,7 @@ function writeCsv(generalDesc, detailsDesc) {
         Longitude: v.Property.Address.Longitude,
         Latitude: v.Property.Address.Latitude,
         PostalCode: u.PostalCode,
-        NeighbourHood: v.Property.Address.Neighbourhood,
+        NeighbourHood: v.Property.Address.CommunityName || v.Property.Address.Neighbourhood,
         Price: u.Property.ConvertedPrice,
         PropertyType: u.Property.Type,
         ParkingSpace: u.Property.ParkingSpaceTotal,
@@ -127,19 +149,39 @@ function writeCsv(generalDesc, detailsDesc) {
         AmmenitiesNearBy: v.Property.AmmenitiesNearBy,
         PropertyTax: v.Property.TaxAmount,
         ZoningDescription: v.Property.ZoningDescription,
+        LowResPhoto: photoPath
     };
 
     const csv = parser.parse(formatted);
     ws.write(csv);
     ws.write('\n');
     //console.log(json);
+
+    if (photoPath) {
+        ImageDownloader.image({
+            url: photoPath,
+            dest: './images/'
+        })
+            .then(msg => console.log('Downlaoded file' + msg))
+            .catch(err => console.log(err))
+    }
 }
+
+var download = function(uri, filename, callback){
+    request.head(uri, function(err, res, body){
+        console.log('content-type:', res.headers['content-type']);
+        console.log('content-length:', res.headers['content-length']);
+
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    });
+};
 
 for (let i=1; i < 51; i++) {
     const json = await getPage(i);
 
     for (let j = 0; j < json.length; j++) {
         const details = await getDetailsRealtorCA(json[j].Id, json[j].MlsNumber);
+
         // const locationDetails = await getLocationScores(
         //     details.Property.Address.Latitude,
         //     details.Property.Address.Longitude);
